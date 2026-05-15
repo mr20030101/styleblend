739b90a6fbe6daf83a95d4963c31e573aa63684f
shean <shean@nextclickpartners.com>
2026-05-15 12:29:07 +0800

    feat: update CategorySeeder to include detailed category descriptions and slugs

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,19 +4,36 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
         $categories = [
-            ['name' => 'Women', 'slug' => 'women', 'description' => 'Women clothing'],
-            ['name' => 'Men', 'slug' => 'men', 'description' => 'Men clothing'],
-            ['name' => 'Kids', 'slug' => 'kids', 'description' => 'Kids clothing'],
+            'Dress',
+            'T-Shirt',
+            'Polo',
+            'Blouse',
+            'Tops',
+            'Pants',
+            'Shorts',
+            'Skirt',
+            'Jacket',
+            'Hoodie',
+            'Swimwear',
+            'Lingerie',
+            'Shoes',
+            'Bag',
+            'Accessories',
+            'Uncategorized',
         ];
 
-        foreach ($categories as $cat) {
-            Category::firstOrCreate(['slug' => $cat['slug']], $cat);
+        foreach ($categories as $name) {
+            Category::firstOrCreate(
+                ['slug' => Str::slug($name)],
+                ['name' => $name, 'is_active' => true]
+            );
         }
     }
 }
